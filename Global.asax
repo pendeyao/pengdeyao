769fc4abb2666a29b8b7e81f4da8234a4d3fb5af
pengdeyao <824348287@qq.com>
2013-04-02 10:39:16 +0800

    ddd

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="my_first.MvcApplication" Language="VB" %>
---
+++ Global.asax.vb
@@ -0,0 +1,27 @@
+﻿' 注意: 有关启用 IIS6 或 IIS7 经典模式的说明，
+' 请访问 http://go.microsoft.com/?LinkId=9394802
+
+Public Class MvcApplication
+    Inherits System.Web.HttpApplication
+
+    Shared Sub RegisterRoutes(ByVal routes As RouteCollection)
+        routes.IgnoreRoute("{resource}.axd/{*pathInfo}")
+
+        ' MapRoute 按顺序采用以下参数:
+        ' (1) 路由名称
+        ' (2) 带参数的 URL
+        ' (3) 参数默认值
+        routes.MapRoute( _
+            "Default", _
+            "{controller}/{action}/{id}", _
+            New With {.controller = "Home", .action = "Index", .id = UrlParameter.Optional} _
+        )
+
+    End Sub
+
+    Sub Application_Start()
+        AreaRegistration.RegisterAllAreas()
+
+        RegisterRoutes(RouteTable.Routes)
+    End Sub
+End Class
